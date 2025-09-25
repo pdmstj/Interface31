92cb4b4c6095cefe47fb408cf0501a030889388b
EunSeo <jpmdyh21@gmail.com>
2025-09-25 09:23:28 +0900

    product 비교 - 불가로 예외 발생

--- Program.cs
+++ Program.cs
@@ -8,9 +8,34 @@ namespace Interface31
 {
     internal class Program
     {
+
+        class product
+        {
+           public string Name { get;  set; }
+           public int Price { get;  set; }
+           
+            public override string ToString()
+            {
+                //return Name + " : " + Price + "원";
+                return $"{Name} : {Price}원";
+            }
+        }
         static void Main(string[] args)
         {
-
+            List<product> products = new List<product>()
+            {
+                new product() { Name = "고구마", Price = 1000 },
+                new product() { Name = "감자", Price = 2000 },
+                new product() { Name = "옥수수", Price = 3000 },
+                new product() { Name = "수박", Price = 4000 },
+                new product() { Name = "참외", Price = 5000 },
+            };
+            //정렬
+            product.Sort();
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
